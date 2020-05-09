efa8d9affcb09128fc76feee2558ca35e97a6549
Maria-Elena Dobrovolschi <marilenadobrovolschi10@gmail.com>
2020-05-09 17:54:03 +0300

    Update StaticBlock.java

--- StaticBlock.java
+++ StaticBlock.java
@@ -1,10 +1,10 @@
-Static initialization blocks are executed when the class is loaded, and you can initialize static variables in those blocks.
+/*Static initialization blocks are executed when the class is loaded, and you can initialize static variables in those blocks.
 
 It's time to test your knowledge of Static initialization blocks. You can read about it here.
 
 You are given a class Solution with a main method. Complete the given code so that it outputs the area of a parallelogram with breadth  and height . You should read the variables from the standard input.
 
-If  or  , the output should be "java.lang.Exception: Breadth and height must be positive" without quotes.
+If  or  , the output should be "java.lang.Exception: Breadth and height must be positive" without quotes.*/
 import java.io.*;
 import java.util.*;
 import java.text.*;
