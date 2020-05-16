d446010c3a7b13ba1851176460785ecd6dc67ffb
Maria-Elena Dobrovolschi <marilenadobrovolschi10@gmail.com>
2020-05-16 12:42:19 +0300

    Create Prime_BigInteger.java

---
+++ Prime_BigInteger.java
@@ -0,0 +1,34 @@
+/*
+A prime number is a natural number greater than  whose only positive divisors are  and itself. For example, the first six prime numbers are , , , , , and .
+
+Given a large integer, , use the Java BigInteger class' isProbablePrime method to determine and print whether it's prime or not prime.
+
+Input Format
+
+A single line containing an integer,  (the number to be checked).
+*/
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        String n = scanner.nextLine();
+        BigInteger k=new BigInteger(n);
+        boolean result;
+        result=k.isProbablePrime(1);
+        if(result)
+        System.out.println("prime");
+        else System.out.println("not prime");
+        scanner.close();
+    }
+}
