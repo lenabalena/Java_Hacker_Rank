4c33c68ee91d2bdc223d5e821c483b36660ded8e
Maria-Elena Dobrovolschi <marilenadobrovolschi10@gmail.com>
2020-05-18 11:20:08 +0300

    Create 1D_Array_game.java

---
+++ 1D_Array_game.java
@@ -0,0 +1,48 @@
+/*
+Let's play a game on an array! You're standing at index  of an -element array named . From some index  (where ), you can perform one of the following moves:
+
+Move Backward: If cell  exists and contains a , you can walk back to cell .
+Move Forward:
+If cell  contains a zero, you can walk to cell .
+If cell  contains a zero, you can jump to cell .
+If you're standing in cell  or the value of , you can walk or jump off the end of the array and win the game.
+In other words, you can move from index  to index , , or  as long as the destination index is a cell containing a . If the destination index is greater than , you win the game.
+
+Given  and , complete the function in the editor below so that it returns true if you can win the game (or false if you cannot).
+*/
+import java.util.*;
+
+public class Solution {
+
+    public static boolean canWin(int leap, int[] game) {
+        if(game==null) return false;
+        return isSolvable(leap,game,0);}
+
+    public static boolean isSolvable(int leap,int[] game,int i){  
+        if(i>=game.length)return true;
+        else 
+        if(i<0 || game[i]==1) return false;
+
+        game[i]=1;
+        return isSolvable(leap,game,i+1)||isSolvable(leap,game,i+leap)||isSolvable(leap,game,i-1);
+        
+
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int q = scan.nextInt();
+        while (q-- > 0) {
+            int n = scan.nextInt();
+            int leap = scan.nextInt();
+            
+            int[] game = new int[n];
+            for (int i = 0; i < n; i++) {
+                game[i] = scan.nextInt();
+            }
+
+            System.out.println( (canWin(leap, game)) ? "YES" : "NO" );
+        }
+        scan.close();
+    }
+}
