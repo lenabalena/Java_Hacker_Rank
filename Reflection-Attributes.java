0381d5b19d8299819d07edbd5dd84ac36ae17604
Maria-Elena Dobrovolschi <marilenadobrovolschi10@gmail.com>
2020-05-26 13:08:31 +0300

    Create Reflection-Attributes.java

---
+++ Reflection-Attributes.java
@@ -0,0 +1,23 @@
+/*
+JAVA reflection is a very powerful tool to inspect the attributes of a class in runtime. For example, we can retrieve the list of public fields of a class using getDeclaredMethods().
+
+In this problem, you will be given a class Solution in the editor. You have to fill in the incompleted lines so that it prints all the methods of another class called Student in alphabetical order. We will append your code with the Student class before running it.
+*/
+public class Solution {
+
+        public static void main(String[] args){
+            
+            Class s = Student.class;
+            Method[] methods = s.getDeclaredMethods();
+
+            ArrayList<String> methodList = new ArrayList<>();
+            for(Method method:methods){
+                methodList.add(method.getName());
+            }
+            Collections.sort(methodList);
+            for(String name: methodList){
+                System.out.println(name);
+            }
+        }
+
+    }
